5e28fd69561806b1167659ab3b4e6fa0c13e594b
Timothy Lien <124855366+TimothyLien@users.noreply.github.com>
2023-08-09 22:24:44 -0400

    Add files via upload

---
+++ Kyle Detection - Copy/Tensorflow Object Detection/TFODCourse/KyleDetection.py
@@ -0,0 +1,155 @@
+import os
+import tensorflow as tf
+import object_detection
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as viz_utils
+from object_detection.builders import model_builder
+from object_detection.utils import config_util
+
+import cv2 
+import numpy as np
+from matplotlib import pyplot as plt
+import smtplib
+from email.message import EmailMessage
+
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+os.chdir('c:\\Users\\timli\\Kyle Detection\\Tensorflow Object Detection\\TFODCourse')
+
+CUSTOM_MODEL_NAME = 'my_ssd_mobnet_tuned' 
+PRETRAINED_MODEL_NAME = 'ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8'
+PRETRAINED_MODEL_URL = 'http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8.tar.gz'
+TF_RECORD_SCRIPT_NAME = 'generate_tfrecord.py'
+LABEL_MAP_NAME = 'label_map.pbtxt'
+
+paths = {
+    'WORKSPACE_PATH': os.path.join('Tensorflow', 'workspace'),
+    'SCRIPTS_PATH': os.path.join('Tensorflow','scripts'),
+    'APIMODEL_PATH': os.path.join('Tensorflow','models'),
+    'ANNOTATION_PATH': os.path.join('Tensorflow', 'workspace','annotations'),
+    'IMAGE_PATH': os.path.join('Tensorflow', 'workspace','images'),
+    'MODEL_PATH': os.path.join('Tensorflow', 'workspace','models'),
+    'PRETRAINED_MODEL_PATH': os.path.join('Tensorflow', 'workspace','pre-trained-models'),
+    'CHECKPOINT_PATH': os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME), 
+    'OUTPUT_PATH': os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'export'), 
+    'TFJS_PATH':os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'tfjsexport'), 
+    'TFLITE_PATH':os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'tfliteexport'), 
+    'PROTOC_PATH':os.path.join('Tensorflow','protoc')
+ }
+
+files = {
+    'PIPELINE_CONFIG':os.path.join('Tensorflow', 'workspace','models', CUSTOM_MODEL_NAME, 'pipeline.config'),
+    'TF_RECORD_SCRIPT': os.path.join(paths['SCRIPTS_PATH'], TF_RECORD_SCRIPT_NAME), 
+    'LABELMAP': os.path.join(paths['ANNOTATION_PATH'], LABEL_MAP_NAME)
+}
+
+# Load pipeline config and build a detection model
+configs = config_util.get_configs_from_pipeline_file(files['PIPELINE_CONFIG'])
+detection_model = model_builder.build(model_config=configs['model'], is_training=False)
+
+# Restore checkpoint
+ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
+ckpt.restore(os.path.join(paths['CHECKPOINT_PATH'], 'ckpt-5')).expect_partial()
+
+@tf.function
+def detect_fn(image):
+    image, shapes = detection_model.preprocess(image)
+    prediction_dict = detection_model.predict(image, shapes)
+    detections = detection_model.postprocess(prediction_dict, shapes)
+    return detections
+
+
+
+
+category_index = label_map_util.create_category_index_from_labelmap(files['LABELMAP'])
+
+def email_alert(subject, body, to):
+    msg = EmailMessage()
+    msg.set_content(body)
+    msg['subject'] = subject
+    msg['to'] = to
+
+    user = "timlien13@gmail.com"
+    msg['from'] = user
+    password = "adrqmffhziypyqqp"
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(user, password)
+    server.send_message(msg)
+    server.quit()
+
+sleepCount = 0
+while True:
+    gauth = GoogleAuth()
+    # Try to load saved client credentials
+    gauth.LoadCredentialsFile("mycreds.txt")
+    if gauth.credentials is None:
+        # Authenticate if they're not there
+        gauth.LocalWebserverAuth()
+    elif gauth.access_token_expired:
+        # Refresh them if expired
+        gauth.Refresh()
+    else:
+        # Initialize the saved creds
+        gauth.Authorize()
+    # Save the current credentials to a file
+    gauth.SaveCredentialsFile("mycreds.txt")
+    drive = GoogleDrive(gauth)
+
+
+    fileList = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+    fileID = 0
+    for file in fileList:
+    # Get the folder ID that you want
+        if(file['title'] == "DetectionImage.png"):
+            print('Title: %s, ID: %s' % (file['title'], file['id']))
+            fileID = file['id']
+
+    if fileID:
+        file1 = drive.CreateFile({'id': fileID})
+        file1.GetContentFile(file1['title'])
+        file1.Trash()
+
+        IMAGE_PATH = 'DetectionImage.png'
+
+        img = cv2.imread(IMAGE_PATH)
+        image_np = np.array(img)
+
+        input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
+        detections = detect_fn(input_tensor)
+
+        num_detections = int(detections.pop('num_detections'))
+        detections = {key: value[0, :num_detections].numpy()
+                    for key, value in detections.items()}
+        detections['num_detections'] = num_detections
+
+        # detection_classes should be ints.
+        detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+        label_id_offset = 1
+        image_np_with_detections = image_np.copy()
+
+        viz_utils.visualize_boxes_and_labels_on_image_array(
+                    image_np_with_detections,
+                    detections['detection_boxes'],
+                    detections['detection_classes']+label_id_offset,
+                    detections['detection_scores'],
+                    category_index,
+                    use_normalized_coordinates=True,
+                    max_boxes_to_draw=5,
+                    min_score_thresh=0.8,
+                    agnostic_mode=False)
+
+        if category_index[detections['detection_classes'][0]+label_id_offset]["name"] == "asleep":
+            sleepCount += 1
+            if sleepCount == 3:
+                email_alert("Kyle Alarm", "Kyle is asleep", "9733931596@txt.att.net")
+                email_alert("Kyle Alarm", "Kyle is asleep", "2019536449@vtext.com")
+                email_alert("Kyle Alarm", "Kyle is asleep", "2014073972@txt.att.net")
+        else:
+            sleepCount = 0
+        print(sleepCount)
+        fileID = None
+        
